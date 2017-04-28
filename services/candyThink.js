e0a21c9fee9b34ded4c7ea5abdb22f6b1655fa05
DaichiMiyajima <let_it_be7daichi@yahoo.co.jp>
2017-04-28 15:46:22 +0900

    commit logic of arbitrage
    localデータでテスト済み

--- services/candyThink.js
+++ services/candyThink.js
@@ -1,12 +1,18 @@
 var _ = require('underscore');
 
+
 var candyThink = function(storage){
 
     this.storage = storage;
 
     _.bindAll(this,
-        'arbitrage'
+        'arbitrage',
+        'orderpush',
+        'orderclear'
     );
+
+    this.order = [];
+
 }
 
 //---EventEmitter Setup
@@ -15,74 +21,147 @@ var EventEmitter = require('events').EventEmitter;
 Util.inherits(candyThink, EventEmitter);
 //---EventEmitter Setup
 
+/* arbitrage 
+** 5分に1回全ての板情報を計算する。都度計算は行わない。
+*/
 candyThink.prototype.arbitrage = function(boards){
 
-    /*
-        board = [{
-            time: moment().format("YYYY-MM-DD HH:mm:ss"),
-            asks: response.result.XETHXXBT.asks,
-            bids: response.result.XETHXXBT.bids
-            name: 'kralen'
-        },
-        {   
-            time: moment().format("YYYY-MM-DD HH:mm:ss"),
-            bids: response.bids,
-            asks: response.asks,
-            name: 'bitflyer'
-        }];
-        // bids以下は例えば・・・
-        bids = [{
-            price : 0.0395,
-            size : 2.5
-            },
-            {
-            price : 0.0391,
-            size : 11
-            }....
-        ]
-    */
+    //order情報の初期値をセット
+    var order = null
+    //1.ask(売注文 =買える)算出
+    var boardsAsk = boards;
+    //filter ask Board
+    var boardsAsk_filter = 
+        _.filter(boardsAsk, function(askboards){
+            return (
+                //askのデータを抽出
+                askboards.ask_bid == "ask"
+                //bidのmax値 > askのamountを抽出
+                && askboards.amount <
+                    _.max(
+                        //firstArgument:list bidで絞り込んだboard
+                        _.filter(boards, function(boards){
+                            return (boards.ask_bid == "bid") 
+                        }),
+                        //関数内のreturnで返した値(amount)を軸として最大値だったオブジェクトを返す。
+                        function(bidboard){
+                            return bidboard.amount;
+                        }
+                    ).amount
+            ) 
+        });
+    //order by asc of amount
+    boardsAsk_filter = _.sortBy(boardsAsk_filter, 'amount');
 
-    console.log('board[0] exchange = ' + boards[0].name + '. Time = ' + boards[0].time);
-    console.log('board[1] exchange = ' + boards[1].name + '. Time = ' + boards[1].time);
-
-    var order = [
-        {
-            exchange : 'bitflyer',
-            child_order_type: "LIMIT",
-            side: "BUY",
-            price: 0,
-            size: 0,
-            minute_to_expire: 0,
-            time_in_force: "GTC"
-        },
-        {
-            exchange : 'kraken',
-            child_order_type: "LIMIT",
-            side: "sell",
-            price: 0,
-            size: 0,
-            minute_to_expire: 0,
-            time_in_force: "GTC"
-        }
-
-    ];
+    //2.ask_bid比較
+    var boardsBid = boards;
+    if(_.size(boardsAsk_filter) == 0){
+        order = null
+    }else{
+        _.every(boardsAsk_filter, function(eachboardAsk){
+            //2.1.bid算出
+            var boardsBid_filter = 
+                _.filter(boardsBid, function(bidboards){
+                    return (
+                        //bidのデータを抽出
+                        bidboards.ask_bid == "bid"
+                        //bidのamout > 1.ask(売注文 =買える)算出で取得したamountを抽出
+                        && bidboards.amount > eachboardAsk.amount
+                    ) 
+                });
+            //order by desc of amount
+            boardsBid_filter = _.sortBy(boardsBid_filter, 'amount').reverse();
+            if(_.size(boardsBid_filter) == 0){
+                return false
+            }else{
+                _.every(boardsBid_filter, function(eachboardsBid){
+                    if(eachboardAsk.num > eachboardsBid.num ){
+                    
+                        //①-1 Order : 2.1.bid算出で算出したデータの売注文(bidを売る)をテーブルに登録する
+                        //①-2 Order : 1.ask算出で算出したデータの買注文(askを買う)をテーブルに登録する(売注文(bid)と同じ量)
+                        this.orderpush(eachboardAsk,eachboardsBid,eachboardsBid.num);
+                        
+                        //②bidのamout = 0 に更新  次の"2.1.bid算出"時に条件にかからないようにamount = 0とする
+                        _.where(boardsBid, {no: eachboardsBid.no})[0].amount = 0;
+                        
+                        //③[ask_num] = [ask_num] - [bid_num]
+                        eachboardAsk.num = eachboardAsk.num - eachboardsBid.num;
+                        
+                    }else if(eachboardAsk.num < eachboardsBid.num ){
+                    
+                        //①-1 Order : 1.ask算出で算出したデータの買注文(askを買う)をテーブルに登録する
+                        //①-1 Order : 2.1.bid算出で算出したデータの売注文(bidを売る)をテーブルに登録する(買注文(ask)と同じ量)
+                        this.orderpush(eachboardAsk,eachboardsBid,eachboardAsk.num);
+                        
+                        //②bidのnum =  [bid_num] - [ask_num] に更新
+                        _.where(boardsBid, {no: eachboardsBid.no})[0].num = eachboardsBid.num - eachboardAsk.num;
+                        
+                        //break this loop
+                        return false;
+                    }else if(eachboardAsk.num == eachboardsBid.num ){
+                        //①-1 Order : 2.1.bid算出で算出したデータの売注文(bidを売る)をテーブルに登録する
+                        //①-2 Order : 1.ask算出で算出したデータの買注文(askを買う)をテーブルに登録する(売注文(bid)と同じ量)
+                        this.orderpush(eachboardAsk,eachboardsBid,eachboardsBid.num);
+                        
+                        //②bidのamout = 0 に更新  次の"2.1.bid算出"時に条件にかからないようにamount = 0とする
+                        _.where(boardsBid, {no: eachboardsBid.no})[0].amount = 0;
+                        
+                        //break this loop
+                        return false;
+                    }
+                    return true;
+                }.bind(this));
+                return true;
+            }
+        }.bind(this));
+    }
 
     // orderがなければnullをorderにセット
-    this.emit('update', order);
+    this.emit('update', this.order);
+
+    //orderをclear
+    this.orderclear();
 
 }
 
-/* Just an example
-candyThink.prototype.mySQLCB = function(err, result){
-    if(err){
-        console.log(err);
-    }else{
-        console.log('--- results ---');
-	    console.log(result);
-        this.sqlResult = result;
-    }
-});
 
+/* orderpush 
+** orderを追加
 */
+candyThink.prototype.orderpush = function(eachboardAsk,eachboardsBid,num){
+
+    var ask_order = 
+        {
+            exchange : eachboardAsk.exchange,
+            child_order_type: "LIMIT",
+            side: "BUY",
+            price: eachboardAsk.amount,
+            size: num,
+            minute_to_expire: 0,
+            time_in_force: "GTC"
+        }
+    var bid_order = 
+        {
+            exchange : eachboardsBid.exchange,
+            child_order_type: "LIMIT",
+            side: "SELL",
+            price: eachboardsBid.amount,
+            size: num,
+            minute_to_expire: 0,
+            time_in_force: "GTC"
+        }
+    this.order.push(ask_order);
+    this.order.push(bid_order);
+
+}
+
+/* orderclear
+** orderをclear
+*/
+candyThink.prototype.orderclear = function(){
+
+    this.order.length = 0;
+
+}
 
 module.exports = candyThink;
